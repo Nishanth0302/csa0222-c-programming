c83516483eaf0e7c7d2cadd0b09c7876b0491d80
Nishanth Reddy <132687006+Nishanth0302@users.noreply.github.com>
2023-09-11 10:37:49 +0530

    Update 10 . factors.cpp

--- 10 . factors.cpp
+++ 10 . factors.cpp
@@ -1,14 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int i,n,count=0;
-	printf("enter the number:");
+	int i,n;
 	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
+	for(i=1;i<=n;i++){
 		if(n%i==0)
-		count++;
+		printf(" %d",i);
 	}
-	printf("number of factors for %d is %d",n,count);
 	return 0;
 }
